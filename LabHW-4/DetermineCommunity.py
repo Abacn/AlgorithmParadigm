4fddf0f4083bbd0fb998fcf02e58986d63016862
Tianyu Dai <td115@duke.edu>
2018-12-02 23:42:05 -0500

    clean up

--- LabHW-4/DetermineCommunity.py
+++ LabHW-4/DetermineCommunity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Test 4-5"""
 import numpy as np
 import math
 from ReadGraph import * 
@@ -14,49 +14,45 @@ def vectorDistance(v1, v2):
 def vertexRepresentation(M, k=20): 
     ver_rep = []
     lambda_s, v_s = smallEigenV(M, k)
+    """Discard the eigenvalue zero,as in Test 1, we find there is only one eigenvalue 0"""
+    lambda_s = lambda_s[1:]
+    v_s = v_s[:, 1:]
     for rp in range(k):
         ver_rep.append(v_s[:, rp])
     ver_rep = list(map(list, zip(*ver_rep)))
     return ver_rep
 
-"""Find the fisrt m closest vertices to the given vertex g"""
+"""Find the fisrt m closest vertices to the given vertex g, and then find the lowest conductance cut from m vertices"""
 def closestVertices(L, g, m, adj_matrix): 
     ver_rep = vertexRepresentation(L)
     distance_g = [vectorDistance(ver_rep[g], ver) for ver in ver_rep]
 
-	"""Index of the clostest m vertices to g (include the index of g)"""
-	# sorted_distance_g = np.sort(distance_g)
-	# print(min(distance_g))
-	clostest_m = sorted(range(len(distance_g)), key=lambda k: distance_g[k])[:m]
-#	for i in clostest_m: 
-#		print(distance_g[i])
-	#clostest_m = [int(np.argwhere(distance_g==distance)) for distance in sorted_distance_g[:m]]
-	S = []
-	conductance = []
-	print(clostest_m)
-	for ver in clostest_m: 
-		S.append(ver)
-		print(adj_matrix[g][ver])
-		conductance.append(cutConductance(S, adj_matrix))
-	min_conductance = min(conductance[9:])
-	min_index = conductance.index(min_conductance)
-	print(min_index)
-	community = S[:min_index+1]
-	return community
-	
+    """Index of the clostest m vertices to g (include the index of g)"""
+    clostest_m = sorted(range(len(distance_g)), key=lambda k: distance_g[k])[:m]
+    min_conductance, cut_size = lowConductanceCut(clostest_m, adj_matrix, 10, 110)
+    community = clostest_m[:cut_size]
+    return community
+    
 if __name__ == '__main__': 
     adj_matrix, dep_label = readData()
     size = np.shape(adj_matrix)[0]
     L = buildLaplacian(adj_matrix)
-    """Find the community that the 0 researcher belongs to"""
+
+    """Find the community that the researcher 0 belongs to"""
     community_0 = closestVertices(L, 0, 110, adj_matrix)
     actual_community_0 = [researcher for researcher in community_0 if dep_label[researcher] == dep_label[0]]
     fraction_0 = len(actual_community_0) / len(community_0)
-    print("The fraction of the researcher 0 is %.6f" %fraction_0)
+    print("The size of the community that researcher 0 belongs to is %d" %len(community_0))
+    print("The fraction of the correct prediction for researcher 0 is %.6f" %fraction_0)
+    print("Community 0: ")
+    print(", ".join("%d" %researcher for researcher in community_0))
     
     """Find the community that the researcher 7 belongs to"""
     community_7 = closestVertices(L, 7, 110, adj_matrix)
     actual_community_7 = [researcher for researcher in community_7 if dep_label[researcher] == dep_label[7]]
     fraction_7 = len(actual_community_7) / len(community_7)
-    print("The fraction of the researcher 7 is %.6f" %fraction_7)
+    print("\nThe size of the community that researcher 7 belongs to is %d" %len(community_7))
+    print("The fraction of the correct prediction for the researcher 7 is %.6f" %fraction_7)
+    print("Community 7: ")
+    print(", ".join("%d" %researcher for researcher in community_7))
     
--- LabHW-4/MeasureConductance.py
+++ LabHW-4/MeasureConductance.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Test 1-3"""
 import numpy as np
 import random
 from ReadGraph import *
@@ -9,16 +9,30 @@ def randomSample(size, nv):
     sample = random.sample(range(size), nv)
     return sample
     
+def buildCut(V, adj_matrix, size_i, size_f): 
+    print(min(V), max(V))
+    sorted_V_index = sorted(range(len(V)), key=lambda k: V[k])
+    return lowConductanceCut(sorted_V_index, adj_matrix, size_i, size_f)
+
 if __name__ == '__main__': 
     number_samples = 5
     number_vertices = 200
     adj_matrix, dep_label = readData()
     size = np.shape(adj_matrix)[0]
-    conductance = []
-    for i in range(number_samples): 
-        conductance.append(cutConductance(randomSample(size, number_vertices), adj_matrix))
+    L = buildLaplacian(adj_matrix)
+    
+    """Ten smallest eigenvalues of Laplacian"""
+    small_eig_vals, small_eig_vecs = smallEigenV(L, 10)
+    print("The ten smallest eigenvalues of Laplacian: ")
+    print (", ".join("%.3f" % vals for vals in small_eig_vals))
+
+    """Calculate conductance for several random generated samples"""
+    conductance = [cutConductance(randomSample(size, number_vertices), adj_matrix) for i in range(number_samples)]
     ave_cond = sum(conductance)/len(conductance)
     print('The average conductance of %d ransom sample is %.6f' %(number_samples, ave_cond))
-    L = buildLaplacian(adj_matrix)
-    lambda_s, V = smallEigenV(L, 0)
-    print(buildCut(V, adj_matrix, 100, 400))
+    
+    lambda_s, v_s = smallEigenV(L, 1)
+    lambda_s = lambda_s[1:]
+    v_s = v_s[:, 1:]
+    small_cond, cut_size = buildCut(v_s, adj_matrix, 100, 400)
+    print('The smallest conductance is %.3f, the size of the corresponding cut is %d' %(small_cond, cut_size))
